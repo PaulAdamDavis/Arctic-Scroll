5db32553720d3186b0e62918195aa12abaaad018
Paul Adam Davis <pauladamdavis@gmail.com>
2015-10-14 23:41:09 +0100

    Remove UA sniffing crap
    This isn't 2012 anymore…

--- jquery.arctic_scroll.js
+++ jquery.arctic_scroll.js
@@ -6,21 +6,15 @@
             speed: 500,
             scroll_selector: 'html,body'
         };
-        
-        var options = $.extend(defaults, options),
-            to_scroll = options.scroll_selector,
-            ua = $.browser;
-        
-        if ( ua.msie && ua.version.slice(0,1) == "8" ) {
-            to_scroll = window;
-        } else if ( ua.msie && ua.version.slice(0,1) == "7" ) {
-            to_scroll = window;
-        }
 
-        options.elem.click(function(event){     
+        var options = $.extend(defaults, options),
+            to_scroll = options.scroll_selector;
+
+        options.elem.click(function(event){
             event.preventDefault();
             var offset = ($(this).attr('data-offset')) ? $(this).attr('data-offset') : false,
-                position = ($(this).attr('data-position')) ? $(this).attr('data-position') : false;         
+                position = ($(this).attr('data-position')) ? $(this).attr('data-position') : false;
+
             if (offset) {
                 var toMove = parseInt(offset);
                 $(options.scroll_selector).stop(true, false).animate({scrollTop: ($(this.hash).offset().top + toMove) }, options.speed);
